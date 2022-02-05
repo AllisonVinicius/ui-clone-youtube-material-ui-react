edad438407029c3e826b1271b254455765fc216c
allisonvinicius <allisonvinicius63@gmail.com>
2022-02-05 18:53:57 -0400

    -update theme

--- react-materialui/src/App.js
+++ react-materialui/src/App.js
@@ -1,9 +1,19 @@
-import { Button } from '@material-ui/core';
+import { Button, ThemeProvider } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
 import React from 'react';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+     primary: '#f44336',
+         
+    }
+  });
+
   return (
-      <Button color="secondary">Hello word</Button>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">Hello word</Button>
+    </ThemeProvider>
   );
 }
 
